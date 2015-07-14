ad37506fa1d465ecab43bfbf04cf349649a6c24b
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2015-07-14 07:31:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -94,18 +94,19 @@ for ($x=0; $x<=$crap->width; $x++) {
         </ul>
         <br/>
         </td>
-        </tr>
-        </table>
-        
-        <div style="margin-left:100px; Margin-top:100px;">
+        <td>
+
+        <div style="margin-left:50px; Margin-top:50px;">
         <input id="solve" type="checkbox" onclick="show_solution()"/>
         <label for="solve">Show Solution?</label>
         <br/>
         <br/>
         <input type="submit" value="Generate"/>
-        
         </div>
-
+        </td>
+        </tr>
+        </table>
+        
     </form>
   </body>
 </html>
